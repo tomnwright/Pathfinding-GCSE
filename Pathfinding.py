59b70ec9e92559a9f52a96378f58db9dca30fe07
Tom Wright <tomnwright@outlook.com>
2021-08-08 16:45:22 +0100

    Stopped trying to adjust

--- Pathfinding.py
+++ Pathfinding.py
@@ -1,34 +1,9 @@
 import tkinter,threading#,base64,time,winsound,random,sys,os,ast
 #from tkinter import messagebox,filedialog
 #from ctypes import windll,byref,create_unicode_buffer,create_string_buffer
-GWL_EXSTYLE=-20
-WS_EX_APPWINDOW=0x00040000
-WS_EX_TOOLWINDOW=0x00000080
-def set_appwindow(root):
-    hwnd = windll.user32.GetParent(root.winfo_id())
-    style = windll.user32.GetWindowLongW(hwnd, GWL_EXSTYLE)
-    style = style & ~WS_EX_TOOLWINDOW
-    style = style | WS_EX_APPWINDOW
-    res = windll.user32.SetWindowLongW(hwnd, GWL_EXSTYLE, style)
-    root.wm_withdraw()
-    root.after(10, lambda: root.wm_deiconify())
-    
-FR_PRIVATE  = 0x10
-FR_NOT_ENUM = 0x20
 
-def loadfont(fontpath, private=True, enumerable=False):
-    if isinstance(fontpath, bytes):
-        pathbuf = create_string_buffer(fontpath)
-        AddFontResourceEx = windll.gdi32.AddFontResourceExA
-    elif isinstance(fontpath, str): 
-        pathbuf = create_unicode_buffer(fontpath)
-        AddFontResourceEx = windll.gdi32.AddFontResourceExW
-    else:
-        raise TypeError('fontpath must be of type str or unicode')
 
-    flags = (FR_PRIVATE if private else 0) | (FR_NOT_ENUM if not enumerable else 0)
-    numFontsAdded = AddFontResourceEx(byref(pathbuf), flags, 0)
-    return bool(numFontsAdded)
+
 class AStar:
     class Node:
         def __init__(self,node,g,h,parent=None):
@@ -725,5 +700,5 @@ class infowindow(tkinter.Frame):
         sounds=['Interface/AUDIO/popoff.wav','Interface/AUDIO/pop.wav']
         winsound.PlaySound(sounds[random.randint(0,1)],winsound.SND_ASYNC)
 app =window(tkinter.Tk())
-set_appwindow(app.master)
+
 app.mainloop()
